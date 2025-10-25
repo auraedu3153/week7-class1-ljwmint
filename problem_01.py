693ac98f06888f2747dafe561bde040334bdb84a
ljwmint <ljw052511@gmail.com>
2025-10-25 21:54:48 +0900

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -1,3 +1,3 @@
 # 요구사항은 https://www.acmicpc.net/problem/1018을 확인하세요
 def min_count_of_squares(board: list[list[str]]) -> int:
-    nboard=[[((board[j][i]=="B")+i+j)%2 for i in range(len(board))] for j in range(len(board[0]))];return 32-abs(32- max([sum([nboard[i+_][j+__] for _ in range(8) for __ in range(8)]) for i in range(len(board)-7) for j in range(len(board[0])-7)]))
+    nboard=[[((board[j][i]=="B")+i+j)%2 for i in range(len(board))] for j in range(len(board[0]))];return 32-abs(32- max([sum([nboard[i+_][j+__] for _ in range(8) for __ in range(8)]) for i in range(len(board)-8) for j in range(len(board[0])-8)]))
